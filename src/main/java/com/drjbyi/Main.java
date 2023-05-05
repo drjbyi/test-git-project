b8012ac65815887ccf40876b3fbeb2a1edc41ae4
drjbyi <132648736+drjbyi@users.noreply.github.com>
2023-05-05 18:15:40 +0800

    Update Main.java
    修改

--- src/main/java/com/drjbyi/Main.java
+++ src/main/java/com/drjbyi/Main.java
@@ -8,6 +8,6 @@ package com.drjbyi;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("砸瓦鲁多!");
     }
 }
