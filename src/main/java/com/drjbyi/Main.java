21bcbf65188ca03ea68a08771e4bcf160a9ecdc2
drjbyi <1291443900@qq.com>
2023-05-05 18:08:28 +0800

    Initial commit

---
+++ src/main/java/com/drjbyi/Main.java
@@ -0,0 +1,13 @@
+package com.drjbyi;
+
+/**
+ * @PROJECT_NAME: ${PROJECT_NAME}
+ * @USER: ${USER}
+ * @DATE: ${DATE} ${TIME}
+ * @description: ${description}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
